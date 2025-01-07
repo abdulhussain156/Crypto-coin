19ee96505596ce8bd6503a71cfa531953553f644
abdulhussain156 <saheel.hussain958@gmail.com>
2025-01-07 17:51:34 +1100

    initial

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,7 +9,7 @@ import { Box } from '@mui/material';
 const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
-      <title>{`${title} | Luliex Admin`}</title>
+      <title>{`${title} | Crypto Coin`}</title>
       {meta}
     </Helmet>
 
